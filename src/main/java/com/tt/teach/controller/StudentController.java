1a1ed4ee06ea52c2012f9b875f0ed091b121c7b2
xuyun6 <qingtian0125smile@163.com>
2018-12-20 11:03:05 +0800

    ijuhygtfr

--- src/main/java/com/tt/teach/controller/StudentController.java
+++ src/main/java/com/tt/teach/controller/StudentController.java
@@ -3,7 +3,9 @@ package com.tt.teach.controller;
 import com.tt.teach.pojo.Student;
 import com.tt.teach.service.StudentService;
 import com.tt.teach.utils.BaseController;
+import com.tt.teach.utils.JsonResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -71,8 +73,32 @@ public class StudentController extends BaseController {
         return list;
    }
 
-    @RequestMapping(value = "/deleteStudent",method = RequestMethod.GET)
-    public String deleteStudent() {
-        return "明儿见";
+    @RequestMapping(value = "/deleteStudent/{stuNo}",method = RequestMethod.DELETE)
+    @ResponseBody
+    public Object deleteStudent(@PathVariable Integer stuNo) {
+        int result=studentService.deleteStudent(stuNo);
+        if(result>0){
+            return JsonResult.ok("删除成功",result);
+        }
+        return JsonResult.no("删除失败",result);
+    }
+
+    @RequestMapping(value = "/updateStudent",method = RequestMethod.POST)
+    public String updateStudent() {
+        String xuehao=getRequest().getParameter("stuNo");
+        Integer stuNo=Integer.parseInt(xuehao);
+        String stuName=getRequest().getParameter("stuName");
+        String stuPwd=getRequest().getParameter("stuPwd");
+        String phone=getRequest().getParameter("phone");
+        Student student=new Student();
+        student.setStudentNo(stuNo);
+        student.setStudentName(stuName);
+        student.setLoginPwd(stuPwd);
+        student.setPhone(phone);
+        int result=studentService.updateStudent(student);
+        if(result>0){
+            return FORWARD+"/stu/student";
+        }
+        return FORWARD+"/stu/student";
     }
 }
--- src/main/java/com/tt/teach/dao/StudentDao.java
+++ src/main/java/com/tt/teach/dao/StudentDao.java
@@ -1,8 +1,10 @@
 package com.tt.teach.dao;
 
 import com.tt.teach.pojo.Student;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +12,15 @@ import java.util.List;
 @Mapper
 @Repository
 public interface StudentDao {
-   @Select("select * from student where studentNo=#{studentNo} and loginPwd=#{loginPwd}")
+    @Select("select * from student where studentNo=#{studentNo} and loginPwd=#{loginPwd}")
     Student doLogin(Student student);
-   @Select("SELECT * FROM student,grade WHERE grade.gradeID=student.gradeId")
+
+    @Select("SELECT * FROM student")
     List<Student> getStudentList();
+
+    @Update("update student set loginPwd=#{loginPwd},studentName=#{studentName},phone=#{phone} where studentNo=#{studentNo}")
+    int updateStudent(Student student);
+
+    @Delete("delete from student where studentNo=#{stuNo}")
+    int deleteStudent(Integer stuNo);
 }
